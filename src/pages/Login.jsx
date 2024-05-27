9130217ba771e877e628bc2b5baf71bbc3f6b481
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-05-26 20:59:42 -0600

    se inicializo la app con react

---
+++ src/pages/Login.jsx
@@ -0,0 +1,18 @@
+
+
+export function Login(){
+    return(
+        <div>
+            <div>
+                <p>imagen del login</p>
+            </div>
+            <div>
+                <h1>Bienvenido</h1>
+                <p>Ingresa sesion</p>
+                <input />
+                <input />
+                <button>Ingresar</button>
+            </div>
+        </div>
+    )
+}
